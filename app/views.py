377204123a8cf5a371055b64618aa30b2c801a4d
Jordan Campbell <jordancampbell.work@gmail.com>
2025-03-05 04:09:14 -0500

    Exercise 4 - Update the /login route

--- app/views.py
+++ app/views.py
@@ -5,25 +5,27 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 ###
 # Routing for your application.
 ###
 
-@app.route('/')
+
+@app.route("/")
 def home():
     """Render website's home page."""
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/about/')
+@app.route("/about/")
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template("about.html", name="Mary Jane")
 
 
-@app.route('/upload', methods=['POST', 'GET'])
+@app.route("/upload", methods=["POST", "GET"])
 def upload():
     # Instantiate your form class
 
@@ -31,33 +33,49 @@ def upload():
     if form.validate_on_submit():
         # Get file data and save to your uploads folder
 
-        flash('File Saved', 'success')
-        return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
+        flash("File Saved", "success")
+        return redirect(
+            url_for("home")
+        )  # Update this to redirect the user to a route that displays all uploaded image files
 
-    return render_template('upload.html')
+    return render_template("upload.html")
 
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route("/login", methods=["POST", "GET"])
 def login():
     form = LoginForm()
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
+    if form.validate_on_submit():
         # Get the username and password values from the form.
 
-        # Using your model, query database for a user based on the username
-        # and password submitted. Remember you need to compare the password hash.
-        # You will need to import the appropriate function to do so.
-        # Then store the result of that query to a `user` variable so it can be
-        # passed to the login_user() method below.
+        username = form.username.data
+        password = form.password.data
 
-        # Gets user id, load into session
-        login_user(user)
+        user = db.session.execute(
+            db.select(UserProfile).filter_by(username=username)
+        ).scalar()
 
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
+        if user is not None and check_password_hash(user.password, password):
+
+            # Using your model, query database for a user based on the username
+            # and password submitted. Remember you need to compare the password hash.
+            # You will need to import the appropriate function to do so.
+            # Then store the result of that query to a `user` variable so it can be
+            # passed to the login_user() method below.
+
+            flash("You have successfully logged in", "success")
+
+            # Gets user id, load into session
+            login_user(user)
+
+            # Remember to flash a message to the user
+            return redirect(
+                url_for("upload")
+            )  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
+
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
@@ -65,23 +83,26 @@ def login():
 def load_user(id):
     return db.session.execute(db.select(UserProfile).filter_by(id=id)).scalar()
 
+
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
 
 # Flash errors from the form if validation fails
 def flash_errors(form):
     for field, errors in form.errors.items():
         for error in errors:
-            flash(u"Error in the %s field - %s" % (
-                getattr(form, field).label.text,
-                error
-), 'danger')
+            flash(
+                "Error in the %s field - %s" % (getattr(form, field).label.text, error),
+                "danger",
+            )
 
-@app.route('/<file_name>.txt')
+
+@app.route("/<file_name>.txt")
 def send_text_file(file_name):
     """Send your static text file."""
-    file_dot_text = file_name + '.txt'
+    file_dot_text = file_name + ".txt"
     return app.send_static_file(file_dot_text)
 
 
@@ -91,12 +112,12 @@ def add_header(response):
     Add headers to both force latest IE rendering engine or Chrome Frame,
     and also to cache the rendered page for 10 minutes.
     """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
+    response.headers["X-UA-Compatible"] = "IE=Edge,chrome=1"
+    response.headers["Cache-Control"] = "public, max-age=0"
     return response
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
