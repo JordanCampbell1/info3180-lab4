2365148e19f5b902822aeb453af91a31513b399a
Jordan Campbell <jordancampbell.work@gmail.com>
2025-03-05 21:48:31 -0500

    Exercise 5 - File Upload Form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms import StringField, PasswordField, FileField
+from wtforms.validators import InputRequired, DataRequired
+from flask_wtf.file import FileAllowed, FileRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired()])
-    password = PasswordField('Password', validators=[InputRequired()])
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+
+
+class UploadForm(FlaskForm):
+    upload_field = FileField(
+        "Upload Field",
+        validators=[
+            FileRequired(),
+            FileAllowed(["jpg", "jpeg", "png"], "Images only!"),
+        ],
+    )
